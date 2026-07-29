4b85a5ea63d8f7860dfa91efe30f1a85b547d171
Luisa Neubauer <luisa.neubauer@utn.com>
2026-07-29 10:18:52 +0200

    removal of @todo, @manually_verified deocrators to make repo publication ready

--- pollution/polluters_utils.py
+++ pollution/polluters_utils.py
@@ -12,14 +12,6 @@ from .data_types import CellType, parse_cell
 
 # Pollution Utils
 
-def manually_verified(func):
-    func.manually_verified = True
-    return func
-
-def todo(func):
-    func.todo = True
-    return func
-
 def _set_polluted_filename(file: CSVFile, filename: str):
     """Keep the CSVFile metadata and XML root filename in sync."""
     file.filename = filename
