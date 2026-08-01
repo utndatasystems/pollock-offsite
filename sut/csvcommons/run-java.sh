d01d76878b8a352d57aa9ca1b5bd8a1e605621d3
stellamoR <sternbergrke@gmail.com>
2026-08-01 11:42:18 +0200

    cleanup, remove unused SuTs

--- sut/csvcommons/run-java.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -f JCSV.class
-javac -cp ./commons-csv-1.9.0.jar:./json-20220924.jar JCSV.java
-java -Xint -cp .:./commons-csv-1.9.0.jar:./json-20220924.jar:./ JCSV
--- sut/opencsv/run-java.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -f JCSV.class
-javac -cp ./opencsv-5.6.jar:./commons-lang3-3.12.0.jar:./json-20220924.jar JCSV.java
-java -Xint -cp .:./opencsv-5.6.jar:./commons-lang3-3.12.0.jar:./json-20220924.jar JCSV
